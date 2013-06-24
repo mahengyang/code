7a7cb3070ab960760a775468845848e7b8d75fa3
mahengyang <mahengyang163@163.com>
2013-06-24 13:31:02 +0800

    refactoring

--- R/tsung_test.R
+++ R/tsung_test.R
@@ -15,43 +15,39 @@ getAllData <- function(data_file){
 }
 tsung_200 <- getAllData('200.txt')
 tsung_request <- getAllData('request.txt')
+
+xlab <- xlab('time')
+guides <- guides(colour = guide_legend(title = 'tsung test number'))
+geom_type <- geom_line()
+common <- list(xlab,guides,geom_type)
+
+save_result <- function(result_name) {
+  result_jpg <- paste(result_path, '/tsung_',result_name, '.jpg', sep='')
+  ggsave(filename=result_jpg,width=15,height=10)
+  print(result_jpg)
+}
 ##perfs_rate
 ggplot(tsung_200,aes(x=V1, y=V2, colour=code)) +
-  geom_line() +
   ggtitle('http code number (200)') +
-  xlab('time') +
   ylab('response number') +
-  guides(colour = guide_legend(title = 'tsung test number'))
-result_jpg <- paste(result_path, '/tsung_http_code_rate.jpg', sep='')
-ggsave(filename=result_jpg,width=15,height=10)
-print(result_jpg)
+  common
+save_result('http_code_rate')
 
 ##http_code_total
 ggplot(tsung_200,aes(x=V1, y=V3, colour=code)) +
-  geom_line() +
   ggtitle('http code total(200)') +
-  xlab('time') +
   ylab('total response number') +
-  guides(colour = guide_legend(title = 'tsung test number'))
-result_jpg <- paste(result_path, '/tsung_http_code_total.jpg', sep='')
-ggsave(filename=result_jpg,width=15,height=10)
-print(result_jpg)
+  common
+save_result('http_code_total')
 ##
 ggplot(data=tsung_request,aes(x=V1,y=V3,colour=code)) + 
-  geom_line() + 
   ggtitle('response time') +
-  xlab('time') +
   ylab('response time(ms)') +
-  guides(colour = guide_legend(title = 'tsung test number'))
-result_jpg <- paste(result_path, '/tsung_perfs_mean.jpg', sep='')
-ggsave(filename=result_jpg,width=15,height=10)
-print(result_jpg)
+  common
+save_result('perfs_mean')
+
 ggplot(data=tsung_request,aes(x=V1,y=V2,colour=code)) + 
-  geom_line() +
   ggtitle('response number (10s)') +
-  xlab('response time') +
   ylab('response number') +
-  guides(colour = guide_legend(title = 'tsung test number'))
-result_jpg <- paste(result_path, '/tsung_perfs_rate.jpg', sep='')
-ggsave(filename=result_jpg,width=15,height=10)
-print(result_jpg)
+  common
+save_result('perfs_rate')
