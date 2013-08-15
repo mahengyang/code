17c1e5802e556f6f2e970014faaca66e5bc5fa14
mahengyang <mahengyang163@163.com>
2013-08-15 12:43:58 +0800

    mv cannt use ""

--- shell/tsung_jmeter.sh
+++ shell/tsung_jmeter.sh
@@ -202,20 +202,17 @@ kill $deamon_pid > /dev/null 2>&1
 end=`date`
 end=`date -d  "$end" +%s`
 
-current_test_code=`ll $tsung_log_path | tail -n 2 | awk -F' ' '/./ {print $9}'`
-current_test_path="$tsung_log_path/$current_test_code"
+current_tsung_code=`ls -l $tsung_log_path | awk -F' ' '{print $6,$7,$8,$9}' | sort | tail -n 1 |cut -d' ' -f4`
+current_tsung_path="$tsung_log_path/$current_tsung_code"
+echo "tsung test path is: $current_tsung_path"
 
 runtime=`expr $end - $begin`
 if [ $runtime -le 60 ]; then
   echo "runtime is $runtime, seems like temp, $temp_report_path will be deleted"
-  rm -rf "$current_test_path"
+  rm -rf "$current_tsung_path"
 else
-  mv "$temp_report_path/*" "$current_test_path"
-  newReport="<a href=\"./log/$current_test_code\"> \
-    $current_test_code -- api:${api%%\?*} -- loop:$loop user:$user duration:$duration thinktime:$thinktime maxuser:$maxuser \
-    </a>\n<br>\n \
-    <img src=\"./log/$current_test_code/images/graphes-Transactions-rate_tn.png\" alt=\"http_code_rate\" />\n \ 
-    <img src=\"./log/$current_test_code/images/graphes-Perfs-mean_tn.png\" alt=\"perfs-meann\" />\n<br>"
+  mv $temp_report_path/* $current_test_path
+  newReport="<a href=\"./log/$current_tsung_code\">$current_tsung_code -- api:${api%%\?*} -- loop:$loop user:$user duration:$duration thinktime:$thinktime maxuser:$maxuser</a>\n<br>\n<img src=\"./log/$current_tsung_code/images/graphes-Transactions-rate_tn.png\" alt=\"http_code_rate\" />\n<img src=\"./log/$current_tsung_code/images/graphes-Perfs-mean_tn.png\" alt=\"perfs-meann\" />\n<br>"
   #insert one line before </body>
   sed -i "/\/body/i\\${newReport}" $HOME/.tsung/index.html
 fi
