12afe9d182eb49d097a2c5ecfcbd884db78fdecc
mahengyang <mahengyang163@163.com>
2013-08-30 10:48:33 +0800

    add my autohotkey

--- shell/generateHtml.sh
+++ shell/generateHtml.sh
@@ -4,11 +4,12 @@ echo "<html><body><h1>welcom to tsung test report</h1>"
 for dirlist in $(ls $desdir)
 do
   if [ -f "$desdir/$dirlist/test.env" ] ; then
-    testenv=`cat "$desdir/$dirlist/test.env" | grep -E "method:|user:|loop:|duration:|thinktime:|maxuser:"`
+    testenv=`cat "$desdir/$dirlist/test.env" | grep -E "user:|loop:|duration:|thinktime:|maxuser:"`
     api=`cat "$desdir/$dirlist/test.env" | grep api:`
+    method=`cat "$desdir/$dirlist/test.env" | grep method:`
     #api contains ?
     api=${api%%\?*}
-    echo "<a href=\"log/$dirlist\">$dirlist -- $api -- `echo $testenv | xargs`</a>"
+    echo "<a href=\"log/$dirlist\">$dirlist -- $method $api -- `echo $testenv | xargs`</a>"
     echo "<br>"
     echo "<img src=\"log/$dirlist/images/graphes-HTTP_CODE-rate_tn.png\" alt=\"http_code_rate\" />"
     echo "<img src=\"log/$dirlist/images/graphes-Perfs-mean_tn.png\" alt=\"perfs-meann\" />"
