3e16649cb31ae85109588c6da3e0d54871fd1ba0
mahengyang <hengyangma@gmail.com>
2013-11-08 00:08:36 +0800

    success

--- ruby/SendXmppMessage.rb
+++ ruby/SendXmppMessage.rb
@@ -10,6 +10,12 @@ class SendXmppMessage
     @http = Net::HTTP.new("LDKJSERVER0014" , 80)
     @redis = Redis.new(:host => "LDKJSERVER0014", :port => 6379)
     @mysql = mysql
+  end
+
+  def send_request(sender,receiver,sharing)
+p "================================"
+p sender,receiver,sharing
+p "================================"
     message = '{
           "to": {
                 "+": ""
@@ -49,50 +55,47 @@ class SendXmppMessage
           "z":"10000035"
           }'
 
-    @message = JSON::Parser.new(message).parse
-    @text = JSON::Parser.new(text).parse
-    @m = JSON::Parser.new(m).parse
+    message = JSON::Parser.new(message).parse
+    text = JSON::Parser.new(text).parse
+    m = JSON::Parser.new(m).parse
 
-    @req_headers = {
+    req_headers = {
       'Authorization' => 'def7c5b70f04287effaaa402ecf9cd51',
       'Content-Type' => 'application/json',
       "cookie" => "authToken=f329ad4748ca160ece0f36f4c8f77469; Path=/",
       "X-DevId" => '111',
       "X-UserTag" => "999901999999"
     }
-  end
 
-  def send_request(sender,receiver,sharing)
     result = @mysql.query("select display_name,auth_token from users where _id=#{sender}")
     sender_info = {}
     result.each{ |row|
       sender_info = row
     }
-    p sender_info
-    p sharing
-    @redis.set('login:',{sender_info["auth_token"] => sender})
-    @req_headers['cookie'] = "authToken=#{sender_info['auth_token']}; Path=/"
+    p "login:",sender_info["auth_token"], sender
+    @redis.hset("login:",sender_info["auth_token"], sender)
+    req_headers['cookie'] = "authToken=#{sender_info['auth_token']}; Path=/"
 
-    @message['to']['+'] = receiver
-    @text["n"] = sender_info['display_name'];
-    @text['z'] = sender
+    message['to']['+'] = receiver
+    text["n"] = sender_info['display_name'];
+    text['z'] = sender
 
-    @text["text"]["oz"] = receiver
-    @text["text"]["pz"] = receiver
-    @text["text"]["cat"] = sharing["cat"].to_s
-    @text["text"]["pkg"] = sharing["pkg"]
-    @text["text"]["th"] = sharing["th"].to_s
-    @text["text"]["t"] = sharing["n"]
+    text["text"]["oz"] = receiver
+    text["text"]["pz"] = receiver
+    text["text"]["cat"] = sharing["cat"].to_s
+    text["text"]["pkg"] = sharing["pkg"]
+    text["text"]["th"] = sharing["th"].to_s
+    text["text"]["t"] = sharing["n"]
+    tmp = JSON::generate(text["text"])
+    text["text"] = tmp
+    m["text"] = JSON::generate(text)
+    p "****************",m,"***************"
+    message["m"] = m
 
-    @text["text"] = JSON::generate(@text["text"])
-    @m["text"] = JSON::generate(@text)
-    p "****************",@m,"***************"
-    @message["m"] = @m
+    response = @http.request_post('/v2/push', JSON::generate(message), req_headers)
 
-    response = @http.request_post('/v2/push', JSON::generate(@message), @req_headers)
-
-    puts "\n==============http header==============\n#{@req_headers}"
-    puts "\n==============http body==============\n#{JSON::generate(@message)}"
+    puts "\n==============http header==============\n#{req_headers}"
+    puts "\n==============http body==============\n#{JSON::generate(message)}"
     puts "\n==============response==============\n#{response}"
     puts "\n==============response code ==============\n#{response.code}"
     puts "\n==============response.body==============\n#{response.body}"
