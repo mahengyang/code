e3f37454c25834ad879c5c809ade31119ed77dea
mahengyang <hengyang.ma@dewmobile.net>
2014-05-15 12:35:09 +0800

    clojure

--- clojure/hello/src/hello/core.clj
+++ clojure/hello/src/hello/core.clj
@@ -1,10 +1,12 @@
 (ns hello.core
   (:gen-class)
-  (:require [monger.core :as mg])
-  (:require [monger.collection :as mc])
-  (:require [clojure.set :as set])
+  (:refer-clojure :exclude [select find sort])
   (:import [com.mongodb MongoOptions ServerAddress]
-  		   [org.bson.types ObjectId]))
+  		   [org.bson.types ObjectId])
+  (:require [monger.core :as mg]
+  			[monger.collection :as mc] 
+  			[clojure.set :as set]
+  			[monger.query :refer :all]))
 
 (mg/connect! { :host "192.168.56.254" :port 44001 })
 (mg/set-db! (mg/get-db "zapya_api"))
@@ -12,29 +14,46 @@
 (defn to-set [args]
 	(if (set? args) args #{args}))
 
-; (def data 
-; 	(with-open [dbc (mc/find "uris")]
-; 		(into [] (map #(do {(.get % "rid") (.get % "zid")}) dbc))))
+(defn read-batch [coll offset batch]
+	(with-collection coll
+		(find {})
+		(fields [:_id :classify])
+		(skip offset)
+		(limit batch)))
+
+(defn destr [a-seq]
+	(map
+		#(let [{app-name :_id, tags :classify} %1] 
+			(if (> (count tags) 0)
+				[app-name (into #{} tags)]))
+		a-seq))
 
 (def data 
-	(with-open [dbc (mc/find "test")]
-		(into [] (map #(do {(.get % "item") (.get % "user")}) dbc))))
-
-(def result 
-	(apply merge-with 
-		#(set/union (to-set %1) (to-set %2)) 
-		data))
+	(let [coll "ma.app.info"
+		batch 10
+		record-number (mc/count coll)
+		offset (range 0 100 batch)]
+		(apply concat 
+			(pmap destr 
+				(pmap #(read-batch coll % batch) offset)))))
 
 (defn sim [s1 s2]
 	(/ (count (set/intersection s1 s2)) 
        (Math/sqrt (* (count s1) (count s2)))))
 
 (defn calculate [result]
-	(for [[master-key master-value] result :when (set? master-value)
-		  [slave-key slave-value] result :when (and (< master-key slave-key) (set? slave-value))]
-	  	(let [sim (sim master-value slave-value)]
-	  		(if (> sim 0.1)
-	  			(mc/insert "clj" {:r1 master-key :r2 slave-key :sim sim})))))
+	(for [[master-key master-value] result 
+			:when (not (nil? master-key))
+		  [slave-key slave-value] result 
+			:when (and 
+		  			(not (nil? slave-key)) 
+		  			(> 0 (.compareTo master-key slave-key)))]
+  		 (let [sim (sim master-value slave-value)]
+  			(if (> sim 0.1)
+  				(do (.start (Thread. #(do (Thread/sleep 10)))))))))
+
+;(println (.getName (Thread/currentThread ))
+(def total-time (time (dorun (calculate data))))
 
 (defn -main [& args] 
-	(println result))
+	(println "main"))
